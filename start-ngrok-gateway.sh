6cb832fa9c77d49e5679e666f792cc31b7bfdc2e
Dreamer169 <dreamer169@github.com>
2026-04-17 20:23:16 +0000

    feat: /v1 gateway alias + ngrok-gateway fix port 8080

--- start-ngrok-gateway.sh
+++ start-ngrok-gateway.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec /usr/local/bin/ngrok http 9090 --config=/root/.config/ngrok-gateway/ngrok.yml --domain=fantasize-outtakes-backpedal.ngrok-free.dev --log=stdout
+exec /usr/local/bin/ngrok http 8080 --config=/root/.config/ngrok-gateway/ngrok.yml --domain=fantasize-outtakes-backpedal.ngrok-free.dev --log=stdout
