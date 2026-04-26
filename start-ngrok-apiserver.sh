5ba177414ead6c9e5ce863317c841fabc38988b6
Dreamer169 <qq2642612945@gmail.com>
2026-04-26 15:42:46 -0400

    chore(ops): xray ss-pool 10850-10859 + CF IP refresh + ngrok-apiserver port 8080->8081

--- start-ngrok-apiserver.sh
+++ start-ngrok-apiserver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec /usr/local/bin/ngrok http 8080 --config=/root/.config/ngrok-sub2api/ngrok.yml --domain=strive-phoney-vocalize.ngrok-free.dev --log=stdout
+exec /usr/local/bin/ngrok http 8081 --config=/root/.config/ngrok-sub2api/ngrok.yml --domain=strive-phoney-vocalize.ngrok-free.dev --log=stdout
